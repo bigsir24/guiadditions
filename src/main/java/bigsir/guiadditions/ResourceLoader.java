e3fb37475d2a15090906288f18892ad4f942cb07
bigsir24 <169484568+bigsir24@users.noreply.github.com>
2025-04-14 19:02:26 +0200

    Fixed resource loading issues, semantic versioning changes

--- src/main/java/bigsir/guiadditions/ResourceLoader.java
+++ src/main/java/bigsir/guiadditions/ResourceLoader.java
@@ -3,15 +3,17 @@ package bigsir.guiadditions;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import net.fabricmc.loader.api.FabricLoader;
+import org.jetbrains.annotations.Nullable;
 
 import javax.imageio.ImageIO;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Stream;
 
 public class ResourceLoader {
 	private static final String RESOURCE_DIR = FabricLoader.getInstance().getGameDir().toString() + "/guiResources/";
@@ -116,39 +118,75 @@ public class ResourceLoader {
 		}
 	}
 
-	private static void loadDefault(String folder, List<TextureNamed> textures, int width, int height) {
-		Path directory;
+	private static @Nullable URI getUri(URL url) {
 		try {
-			URL url = ResourceLoader.class.getResource("/assets/guiadditions/textures/gui/" + folder + "/");
-			if(url == null) {
-				GuiAdditions.LOGGER.error("Failed to load default textures!");
+			return url == null ? null : url.toURI();
+		} catch (URISyntaxException e) {
+            return null;
+        }
+    }
+
+	private static @Nullable Stream<Path> getPathStreamFixed(String resourcesPath) {
+		URL url = ResourceLoader.class.getResource(resourcesPath);
+		URI uri = getUri(url);
+		if (uri == null) return null;
+
+		Map<String, String> env = new HashMap<>();
+
+		try {
+			return Files.list(Paths.get(uri));
+		} catch (IOException | FileSystemNotFoundException e) {
+			try (FileSystem fs = FileSystems.newFileSystem(uri, env)) {
+				return Files.list(fs.getPath(resourcesPath));
+			} catch (IOException ex) {
+				return null;
+			}
+        }
+	}
+
+	private static void loadTexture (Path path, String folder, List<TextureNamed> textures, int width, int height) {
+		if (isPng(path)) {
+			String fileName = path.getFileName().toString();
+			String textureKey = "guiadditions." + folder + "." + fileName.substring(0, fileName.lastIndexOf(".png"));
+
+			TextureNamed texture;
+			try {
+				texture = new TextureNamed(ImageIO.read(path.toUri().toURL()), textureKey);
+			} catch (IOException e) {
 				return;
 			}
 
-			directory = Paths.get(url.toURI());
-		}catch (URISyntaxException e) {
-			GuiAdditions.LOGGER.error("Failed to load default textures!");
-            return;
-        }
-
-        try (DirectoryStream<Path> paths = Files.newDirectoryStream(directory)) {
-			for (Path path : paths) {
-				if (isPng(path)) {
-					String fileName = path.getFileName().toString();
-					String textureKey = "guiadditions." + folder + "." + fileName.substring(0, fileName.lastIndexOf(".png"));
-
-					TextureNamed texture = new TextureNamed(ImageIO.read(path.toFile()), textureKey);
-
-					if(texture.getWidth() != width || texture.getHeight() != height) {
-						GuiAdditions.LOGGER.error(String.format("%s has incorrect size! Expected: %dx%d", fileName, width, height));
-						texture.delete();
-					}
-
-					textures.add(texture);
-				}
+			if(texture.getWidth() != width || texture.getHeight() != height) {
+				GuiAdditions.LOGGER.error(String.format("%s has incorrect size! Expected: %dx%d", fileName, width, height));
+				texture.delete();
 			}
-		} catch (IOException e) {
-			GuiAdditions.LOGGER.error("Failed to load default textures! (" + folder + ")");
+
+			textures.add(texture);
+		}
+	}
+
+	private static void loadDefault(String folder, List<TextureNamed> textures, int width, int height) {
+		String path = "/assets/guiadditions/textures/gui/" + folder;
+
+		try	(Stream<Path> paths = getPathStreamFixed(path)) {
+			if(paths == null) {
+				GuiAdditions.LOGGER.error("Failed to load resources from " + folder);
+				return;
+			}
+
+			paths.forEach(p -> loadTexture(p, folder, textures, width, height));
+		}
+	}
+
+	private static FileSystem getFileSystem(URI uri) {
+		try {
+			return FileSystems.getFileSystem(uri);
+		}catch (Exception e) {
+			try (FileSystem fs = FileSystems.newFileSystem(uri, Collections.emptyMap())){
+				return fs;
+			} catch (IOException ex) {
+				return  null;
+			}
 		}
 	}
 
