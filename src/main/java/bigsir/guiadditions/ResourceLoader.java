854df0f58ecaad25ead69cdd738b1797915d90b4
bigsir24 <169484568+bigsir24@users.noreply.github.com>
2025-04-17 16:40:55 +0200

    Added support for Zipped resource packs

--- src/main/java/bigsir/guiadditions/ResourceLoader.java
+++ src/main/java/bigsir/guiadditions/ResourceLoader.java
@@ -1,32 +1,53 @@
 package bigsir.guiadditions;
 
+import bigsir.guiadditions.utils.FileSystemFolder;
+import bigsir.guiadditions.utils.ZipUtils;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import net.fabricmc.loader.api.FabricLoader;
-import org.jetbrains.annotations.Nullable;
+import net.fabricmc.loader.api.ModContainer;
 
 import javax.imageio.ImageIO;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.*;
-import java.util.*;
+import java.io.RandomAccessFile;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class ResourceLoader {
 	private static final String RESOURCE_DIR = FabricLoader.getInstance().getGameDir().toString() + "/guiResources/";
 	private static final Gson GSON = new GsonBuilder().create();
-	public static final List<Path> resourceCandidates = new ArrayList<>();
+	public static final List<ResourcePack> resourcePacks = new ArrayList<>();
 	public static final List<TextureNamed> hotbarTextures = new ArrayList<>();
 	public static final List<TextureNamed> slotTextures = new ArrayList<>();
 
 	public static void loadResources() {
 		loadDefault("hotbar", hotbarTextures, 256, 64);
 		loadDefault("slot", slotTextures, 32, 32);
+		hotbarTextures.sort(Comparator.comparingInt(a -> GuiAdditions.getPrecedence(a.getKey())));
+		slotTextures.sort(Comparator.comparingInt(a -> GuiAdditions.getPrecedence(a.getKey())));
 
-		exploreCandidates();
+		for (ResourcePack resourcePack : resourcePacks) {
+			String keyRoot = resourcePack.getKey();
+
+			try (FileSystemFolder fsd = ZipUtils.getNewFileSystem(resourcePack.path)) {
+				if(fsd == null) continue;
+				Path fileSystemPath = fsd.root();
+
+				loadResourceTexture(fileSystemPath, keyRoot, hotbarTextures, "hotbar", 256, 64);
+				loadResourceTexture(fileSystemPath, keyRoot, slotTextures, "slot", 32, 32);
+			} catch (IOException e) {
+                GuiAdditions.LOGGER.error("Failed to load resources for {}!", resourcePack.name);
+            }
+
+		}
     }
 
 	public static void findResourceCandidates() {
@@ -41,113 +62,43 @@ public class ResourceLoader {
 		}
 
 		//Add potential resource packs to list
-		try (DirectoryStream<Path> dirs = Files.newDirectoryStream(resourceDir)) {
-			for (Path p : dirs) {
-				if (Files.isDirectory(p)) resourceCandidates.add(p);
+		try (Stream<Path> stream = Files.list(resourceDir)){
+			List<Path> dirs = stream.collect(Collectors.toList());
+			for (Path path : dirs) {
+				if(Files.isDirectory(path) || isZip(path)) {
+					ResourcePack pack = getResourcePack(path);
+					if(pack != null) resourcePacks.add(pack);
+				}
 			}
 		} catch (IOException e) {
-			GuiAdditions.LOGGER.error("Failed to load resources!");
 		}
 	}
 
-	private static void exploreCandidates() {
-		for (Path resourcePath : resourceCandidates) {
-			Meta meta;
+	private static boolean isZip(Path p) {
+		try (RandomAccessFile file = new RandomAccessFile(p.toFile(), "r")) {
+			int sign = file.readInt();
+			return sign == 0x504B0304;
+		} catch (IOException ignored) {
+			return false;
+		}
+    }
 
-			if ((meta = getMeta(resourcePath)) == null) continue;
+	private static void loadResourceTexture(Path resourcePath, String key, List<TextureNamed> textures, String folder, int width, int height) {
+		Path hotbarDirectory = resourcePath.resolve(folder);
 
-			String keyRoot = getInternalKey(meta);
-
-			loadHotbarTextures(resourcePath, keyRoot);
-			loadSlotTextures(resourcePath, keyRoot);
-        }
-	}
-
-	public static String getInternalKey(Meta meta) {
-		return formattedString(meta.author) + "." + formattedString(meta.name);
-	}
-
-	private static void loadHotbarTextures(Path resourcePath, String key) {
-		Path hotbarDirectory = Paths.get(resourcePath + (resourcePath.endsWith("/") ? "" : "/") + "hotbar/");
 		if (Files.notExists(hotbarDirectory)) return;
 
-		try (DirectoryStream<Path> paths = Files.newDirectoryStream(hotbarDirectory)) {
-			for (Path path : paths) {
-				if (isPng(path)) {
-					String fileName = path.getFileName().toString();
-					String textureKey = key + ".hotbar." + fileName.substring(0, fileName.lastIndexOf(".png"));
-
-					TextureNamed texture = new TextureNamed(ImageIO.read(path.toFile()), textureKey);
-
-					if(texture.getWidth() != 256 || texture.getHeight() != 64) {
-						GuiAdditions.LOGGER.error(fileName + " has incorrect size! Expected: 256x32");
-						texture.delete();
-						continue;
-					}
-
-					hotbarTextures.add(texture);
-				}
-			}
+		try (Stream<Path> paths = Files.list(hotbarDirectory)) {
+			paths.forEach(p -> loadTexture(p, key, folder, textures, width, height));
         } catch (IOException e) {
 			GuiAdditions.LOGGER.error("Failed to load hotbar selector textures!");
 		}
     }
 
-	private static void loadSlotTextures(Path resourcePath, String key) {
-		Path slotDirectory = Paths.get(resourcePath + (resourcePath.endsWith("/") ? "" : "/") + "slot/");
-		if (Files.notExists(slotDirectory)) return;
-
-		try (DirectoryStream<Path> paths = Files.newDirectoryStream(slotDirectory)) {
-			for (Path path : paths) {
-				if (isPng(path)) {
-					String fileName = path.getFileName().toString();
-					String textureKey = key + ".slot." + fileName.substring(0, fileName.lastIndexOf(".png"));
-
-					TextureNamed texture = new TextureNamed(ImageIO.read(path.toFile()), textureKey);
-
-					if(texture.getWidth() != 32 || texture.getHeight() != 32) {
-						GuiAdditions.LOGGER.error(fileName + " has incorrect size! Expected: 32x32");
-						texture.delete();
-					}
-
-					slotTextures.add(texture);
-				}
-			}
-		} catch (IOException e) {
-			GuiAdditions.LOGGER.error("Failed to load slot selector textures!");
-		}
-	}
-
-	private static @Nullable URI getUri(URL url) {
-		try {
-			return url == null ? null : url.toURI();
-		} catch (URISyntaxException e) {
-            return null;
-        }
-    }
-
-	private static @Nullable Stream<Path> getPathStreamFixed(String resourcesPath) {
-		URL url = ResourceLoader.class.getResource(resourcesPath);
-		URI uri = getUri(url);
-		if (uri == null) return null;
-
-		Map<String, String> env = new HashMap<>();
-
-		try {
-			return Files.list(Paths.get(uri));
-		} catch (IOException | FileSystemNotFoundException e) {
-			try (FileSystem fs = FileSystems.newFileSystem(uri, env)) {
-				return Files.list(fs.getPath(resourcesPath));
-			} catch (IOException ex) {
-				return null;
-			}
-        }
-	}
-
-	private static void loadTexture (Path path, String folder, List<TextureNamed> textures, int width, int height) {
+	private static void loadTexture(Path path, String prefix, String folder, List<TextureNamed> textures, int width, int height) {
 		if (isPng(path)) {
 			String fileName = path.getFileName().toString();
-			String textureKey = "guiadditions." + folder + "." + fileName.substring(0, fileName.lastIndexOf(".png"));
+			String textureKey = prefix + "." + folder + "." + fileName.substring(0, fileName.lastIndexOf(".png"));
 
 			TextureNamed texture;
 			try {
@@ -165,30 +116,23 @@ public class ResourceLoader {
 		}
 	}
 
+	@SuppressWarnings("OptionalGetWithoutIsPresent") //This should never fail
 	private static void loadDefault(String folder, List<TextureNamed> textures, int width, int height) {
-		String path = "/assets/guiadditions/textures/gui/" + folder;
+		String path = "assets/guiadditions/textures/gui/" + folder;
 
-		try	(Stream<Path> paths = getPathStreamFixed(path)) {
-			if(paths == null) {
-				GuiAdditions.LOGGER.error("Failed to load resources from " + folder);
-				return;
-			}
-
-			paths.forEach(p -> loadTexture(p, folder, textures, width, height));
+		ModContainer mod = FabricLoader.getInstance().getModContainer(GuiAdditions.MOD_ID).get();
+		Optional<Path> optionalPath = mod.findPath(path);
+		if (!optionalPath.isPresent()) {
+			GuiAdditions.LOGGER.error("Failed to load defaults from {}! (Missing Path)", folder);
+			return;
 		}
-	}
 
-	private static FileSystem getFileSystem(URI uri) {
-		try {
-			return FileSystems.getFileSystem(uri);
-		}catch (Exception e) {
-			try (FileSystem fs = FileSystems.newFileSystem(uri, Collections.emptyMap())){
-				return fs;
-			} catch (IOException ex) {
-				return  null;
-			}
-		}
-	}
+		try	(Stream<Path> paths = Files.list(optionalPath.get())) {
+			paths.forEach(p -> loadTexture(p, GuiAdditions.MOD_ID, folder, textures, width, height));
+		} catch (IOException e) {
+			GuiAdditions.LOGGER.error("Failed to load defaults from {}! (IO Exception)", folder);
+        }
+    }
 
 	private static boolean isPng(Path path) {
 		try {
@@ -198,26 +142,60 @@ public class ResourceLoader {
         }
 	}
 
-	private static String formattedString(String string) {
-		String str = string.replaceAll("[ .\n\t]", "").toLowerCase();
-		return str.substring(0, Math.min(str.length(), 48));
-	}
+	public static ResourcePack getResourcePack(Path path) {
+		try (FileSystemFolder fsd = ZipUtils.getNewFileSystem(path)) {
+			if(fsd == null) return null;
+			Path resourcePath = fsd.root();
 
-	public static Meta getMeta(Path resourcePath) {
-		Path path = Paths.get((resourcePath.endsWith("/") ? resourcePath.toString() : resourcePath + "/") + "meta.json");
-		if (Files.notExists(path)) return null;
+			Path p = resourcePath.resolve("meta.json");
+			if (Files.notExists(p)) return null;
 
-		try (InputStreamReader reader = new InputStreamReader(Files.newInputStream(path))){
-			return GSON.fromJson(reader, Meta.class);
+
+			try (InputStreamReader reader = new InputStreamReader(p.toUri().toURL().openStream())){
+				return GSON.fromJson(reader, ResourcePack.class).withPath(path);
+			} catch (IOException e) {
+				GuiAdditions.LOGGER.error("An error has occurred while loading metadata of {}!", path.getFileName());
+				return null;
+			}
 		} catch (IOException e) {
-			GuiAdditions.LOGGER.error("An error has occurred while loading metadata of " + resourcePath.getFileName() + "!");
+			GuiAdditions.LOGGER.error("An error has occurred while loading metadata of {}", path.getFileName());
 			return null;
-		}
-	}
+        }
 
-	public static class Meta {
-		public String author;
-		public String name;
-		public int version;
+    }
+
+	public static class ResourcePack {
+		private Path path;
+		private final String author;
+		private final String name;
+		private final int version;
+
+		public ResourcePack(String author, String name, int version) {
+			this.author = author;
+			this.name = name;
+			this.version = version;
+		}
+
+		public ResourcePack withPath(Path path) {
+			this.path = path;
+			return this;
+		}
+
+		public Path getPath() {
+			return path;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		private String formattedString(String string) {
+			String str = string.replaceAll("[ .\n\t]", "").toLowerCase();
+			return str.substring(0, Math.min(str.length(), 48));
+		}
+
+		public String getKey() {
+			return formattedString(this.author) + "." + formattedString(this.name);
+		}
 	}
 }
--- src/main/java/bigsir/guiadditions/mixin/I18nMixin.java
+++ src/main/java/bigsir/guiadditions/mixin/I18nMixin.java
@@ -1,6 +1,9 @@
 package bigsir.guiadditions.mixin;
 
+import bigsir.guiadditions.GuiAdditions;
 import bigsir.guiadditions.ResourceLoader;
+import bigsir.guiadditions.utils.FileSystemFolder;
+import bigsir.guiadditions.utils.ZipUtils;
 import io.github.prospector.modmenu.mixin.LanguageAccessor;
 import net.minecraft.core.lang.I18n;
 import net.minecraft.core.lang.Language;
@@ -14,7 +17,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Map;
 import java.util.Properties;
 
@@ -25,10 +27,15 @@ public abstract class I18nMixin {
 
 	@Inject(method = "reload(Ljava/lang/String;Z)V", at = @At("TAIL"))
 	public void load(String languageCode, boolean save, CallbackInfo ci) {
-		for (Path resourcePath : ResourceLoader.resourceCandidates) {
-			Path langPath = Paths.get(resourcePath + "/lang/" + languageCode + ".lang");
-			ResourceLoader.Meta meta = ResourceLoader.getMeta(resourcePath);
-			if(Files.notExists(langPath) || meta == null) continue;
+		String relativePath = "lang/" + languageCode + ".lang";
+		for (ResourceLoader.ResourcePack pack : ResourceLoader.resourcePacks) {
+
+			FileSystemFolder fsd = ZipUtils.getNewFileSystem(pack.getPath());
+			if(fsd == null) continue;
+
+			Path langPath = fsd.resolve(relativePath);
+
+			if(Files.notExists(langPath)) continue;
 
 			try (InputStreamReader reader = new InputStreamReader(langPath.toUri().toURL().openStream())) {
 				Properties tempProperties = new Properties();
@@ -36,12 +43,17 @@ public abstract class I18nMixin {
 				tempProperties.load(reader);
 
 				for (Map.Entry<Object, Object> entry : tempProperties.entrySet()) {
-					currentProperties.setProperty(ResourceLoader.getInternalKey(meta) + "." + entry.getKey(), (String) entry.getValue());
+					currentProperties.setProperty(pack.getKey()+ "." + entry.getKey(), (String) entry.getValue());
 				}
 				((LanguageAccessor)this.currentLanguage).getEntries().load(reader);
 			} catch (IOException e) {
-                throw new RuntimeException(e);
-            }
+				GuiAdditions.LOGGER.error("Failed to load lang files of {}", pack.getName());
+            }finally {
+				try {
+					fsd.close();
+				} catch (IOException e) {
+				}
+			}
         }
 	}
 }
